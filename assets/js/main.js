b356cfa884ec373bd2fcd2e1e9d63241aacf1f32
NOK <nicolas.ortizk@gmail.com>
2025-08-07 23:12:55 -0400

    Remove Swup test preload to avoid phantom history routes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ const swup = new Swup({
 });
 
 // Precargar la página de prueba para acelerar la transición
-swup.preload('/test/');
+// swup.preload('/test/');
 
 // Variable para mantener la instancia del mapa y evitar reinicialización
 let mapInstance = null;
